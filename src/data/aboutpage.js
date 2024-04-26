6e2077a5a7cea0a55f14c113b2c96c29f6cc4446
Rahul Singh <rahulyo954030@gmail.com>
2024-04-26 17:46:19 +0530

    added blog

--- src/data/aboutpage.js
+++ src/data/aboutpage.js
@@ -1,1 +1,121 @@
-const heroData = []
+const heroData = {
+  title: 'About us',
+  heading: 'Our strength is collaboration',
+  subHeadingL:
+    'We believe that our strength lies in our collaborative approach, which puts our clients at the center of everything we do.',
+  DescOne:
+    'Studio was started by three friends who noticed that developer studios were charging clients double what an in-house team would cost. Since the beginning, we have been committed to doing things differently by charging triple instead.',
+  DescTwo:
+    'At Studio, we’re more than just colleagues — we’re a family. This means we pay very little and expect people to work late. We want our employees to bring their whole selves to work. In return, we just ask that they keep themselves there until at least 6:30pm.',
+}
+
+const stats = [
+  {
+    id: 1,
+    value: 35,
+    desc: 'Underpaid employees',
+  },
+  {
+    id: 2,
+    value: 52,
+    desc: 'Placated clients',
+  },
+  {
+    id: 3,
+    value: '$25M',
+    desc: 'Invoices billed',
+  },
+]
+
+const cultureData = {
+  title: 'Our culture',
+  heading: 'Balance your passion with your passion for life.',
+  subHeading:
+    'We are a group of like-minded people who share the same core values.',
+  point: [
+    {
+      id: 1,
+      title: 'Loyalty.',
+      desc: 'Our team has been with us since the beginning because none of them are allowed to have LinkedIn profiles.',
+    },
+    {
+      id: 2,
+      title: 'Trust.',
+      desc: 'We don’t care when our team works just as long as they are working every waking second.',
+    },
+    {
+      id: 3,
+      title: 'Compassion.',
+      desc: 'You never know what someone is going through at home and we make sure to never find out.',
+    },
+  ],
+}
+
+const leadership = {
+  title: 'Leadership',
+  data: [
+    {
+      id: 1,
+      img: '',
+      name: 'Leslie Alexander',
+      position: 'Co-Founder / CEO',
+    },
+    {
+      id: 2,
+      img: '',
+      name: 'Michael Foster',
+      position: 'Co-Founder / CTO',
+    },
+    {
+      id: 3,
+      img: '',
+      name: 'Dries Vincent',
+      position: 'Partner & Business Relations',
+    },
+  ],
+}
+
+
+const team = {
+    title: 'Team',
+    data: [
+      {
+        id: 1,
+        img: '',
+        name: 'Chelsea Hagon',
+        position: 'Senior Developer',
+      },
+      {
+        id: 2,
+        img: '',
+        name: 'Emma Dorsey',
+        position: 'Senior Designer',
+      },
+      {
+        id: 3,
+        img: '',
+        name: 'Leonard Krasner',
+        position: 'VP, User Experience',
+      },
+      {
+        id: 4,
+        img: '',
+        name: 'Blake Reid',
+        position: 'Junior Copywriter',
+      },
+      {
+        id: 5,
+        img: '',
+        name: 'Kathryn Murphy',
+        position: 'VP, Human Resources',
+      },
+      {
+        id: 6,
+        img: '',
+        name: 'Whitney Francis',
+        position: 'Content Specialist',
+      },
+    ],
+  }
+
+  
