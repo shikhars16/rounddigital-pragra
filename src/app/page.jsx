8ec9d2c0d2ceced69fb5d9835adaca8ca94b8f89
Rahul Singh <rahulyo954030@gmail.com>
2024-05-07 14:31:13 +0530

    mapped contact and testimonial

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,13 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import {heroData, clientData, caseStudiesData, testimonialData, servicesData, contactData} from '@/data/homepage'
+import {
+  heroData,
+  clientData,
+  caseStudiesData,
+  testimonialData,
+  servicesData,
+  // contactData,
+} from '@/data/homepage'
 import { ContactSection } from '@/components/ContactSection'
 import { Container } from '@/components/Container'
 import { FadeIn, FadeInStagger } from '@/components/FadeIn'
@@ -16,15 +23,16 @@ import { loadCaseStudies } from '@/lib/mdx'
 import { client, getPosts, urlFor } from '@/utils/sanity'
 // import { useEffect } from 'react'
 
-
-function Clients({clients}) {
+function Clients({ clients }) {
   return (
     <div className="mt-24 rounded-4xl bg-[#e14242] py-20 sm:mt-32 sm:py-32 lg:mt-56">
       <Container>
         <FadeIn className="flex items-center gap-x-8">
-          {clients?.heading && <h2 className="text-center font-display text-sm font-semibold tracking-wider text-white sm:text-left">
-           {clients.heading}
-          </h2>}
+          {clients?.heading && (
+            <h2 className="text-center font-display text-sm font-semibold tracking-wider text-white sm:text-left">
+              {clients.heading}
+            </h2>
+          )}
           <div className="h-px flex-auto bg-neutral-800" />
         </FadeIn>
         <FadeInStagger faster>
@@ -32,13 +40,20 @@ function Clients({clients}) {
             role="list"
             className="mt-10 grid grid-cols-2 gap-x-8 gap-y-10 lg:grid-cols-4"
           >
-            {clients?.clientsArray && clients.clientsArray.map((item) => (
-              <li key={item.key}>
-                <FadeIn>
-                  <Image src={urlFor(item?.image).url()} alt={item.name} unoptimized width={200} height={100}/>
-                </FadeIn>
-              </li>
-            ))}
+            {clients?.clientsArray &&
+              clients.clientsArray.map((item) => (
+                <li key={item.key}>
+                  <FadeIn>
+                    <Image
+                      src={urlFor(item?.image).url()}
+                      alt={item.name}
+                      unoptimized
+                      width={200}
+                      height={100}
+                    />
+                  </FadeIn>
+                </li>
+              ))}
           </ul>
         </FadeInStagger>
       </Container>
@@ -50,12 +65,14 @@ function CaseStudies({ caseStudies }) {
   return (
     <>
       <SectionIntro
-        title={caseStudiesData?.heading ? caseStudiesData.heading :"Harnessing technology for a brighter future"}
+        title={
+          caseStudiesData?.heading
+            ? caseStudiesData.heading
+            : 'Harnessing technology for a brighter future'
+        }
         className="mt-24 sm:mt-32 lg:mt-40"
       >
-        {caseStudiesData?.description  && <p>
-           {caseStudiesData.description}
-        </p>}
+        {caseStudiesData?.description && <p>{caseStudiesData.description}</p>}
       </SectionIntro>
       <Container className="mt-16">
         <FadeInStagger className="grid grid-cols-1 gap-8 lg:grid-cols-3">
@@ -100,37 +117,34 @@ function CaseStudies({ caseStudies }) {
   )
 }
 
-function Services() {
+function Services({ data }) {
   return (
     <>
       <SectionIntro
         eyebrow={servicesData.eyebrow}
-        title={servicesData.heading}
+        title={data.heading}
         className="mt-24 sm:mt-32 lg:mt-40"
       >
-        <p>
-         {servicesData.description}
-        </p>
+        <p>{data.description}</p>
       </SectionIntro>
       <Container className="mt-16">
         <div className="lg:flex lg:items-center lg:justify-end">
           <div className="flex justify-center lg:w-1/2 lg:justify-end lg:pr-12">
             <FadeIn className="w-[33.75rem] flex-none lg:w-[45rem]">
               <StylizedImage
-                src={imageLaptop}
+                src={urlFor(data?.image).url()}
                 sizes="(min-width: 1024px) 41rem, 31rem"
                 className="justify-center lg:justify-end"
               />
             </FadeIn>
           </div>
           <List className="mt-16 lg:mt-0 lg:w-1/2 lg:min-w-[33rem] lg:pl-4">
-           
-           {servicesData?.listData && servicesData.listData.map((item,index) => (
-             <ListItem key={index} title={item.title}>
-              {item.description}
-             </ListItem>
-           ))}
-            
+            {data?.serviceArray &&
+              data.serviceArray.map((item, index) => (
+                <ListItem key={index} title={item.title}>
+                  {item.description}
+                </ListItem>
+              ))}
           </List>
         </div>
       </Container>
@@ -143,51 +157,58 @@ export const metadata = {
     'We are a development studio working at the intersection of design and technology.',
 }
 
-export default async function Home({props}) {
+export default async function Home({ props }) {
   let caseStudies = (await loadCaseStudies()).slice(0, 3)
 
-
-  const clientData =await getSanityData()
-  console.log(clientData[0].clients, 'client data from sanity')
-
+  const clientData = await getSanityData()
+  console.log(
+    'client data from sanity',
+    clientData[0],
+    'client data from sanity',
+  )
 
   return (
     <>
       <Container className="mt-24 sm:mt-32 md:mt-56">
         <FadeIn className="max-w-3xl">
-          {heroData?.heading && <h1 className="font-display text-5xl font-medium tracking-tight text-[#e14242] [text-wrap:balance] sm:text-7xl">
-            {heroData.heading}
-          </h1>}
-          {heroData?.description && <p className="mt-6 text-xl text-neutral-600">
-            {heroData.description}
-            </p>}
+          {clientData[0].heroComponent?.heading && (
+            <h1 className="font-display text-5xl font-medium tracking-tight text-[#e14242] [text-wrap:balance] sm:text-7xl">
+              {clientData[0].heroComponent.heading}
+            </h1>
+          )}
+          {clientData[0].heroComponent?.description && (
+            <p className="mt-6 text-xl text-neutral-600">
+              {clientData[0].heroComponent.description}
+            </p>
+          )}
         </FadeIn>
       </Container>
 
-      <Clients clients={clientData[0].clients}/>
+      <Clients clients={clientData[0].clients} />
 
       <CaseStudies caseStudies={caseStudies} />
 
       <Testimonial
         className="mt-24 sm:mt-32 lg:mt-40"
-        client={{ name: testimonialData.name, logo: testimonialData.logo }}
+        client={{
+          name: clientData[0]?.testimonialData?.name,
+          logo: urlFor(clientData[0].testimonialData.logo).url(),
+        }}
       >
-      {testimonialData.quote}
+        {clientData[0]?.testimonialData?.quote}
       </Testimonial>
 
-      <Services />
+      <Services data={clientData[0].service} />
 
-      <ContactSection contactData={contactData} />
+      <ContactSection contactData={clientData[0].contact} />
     </>
   )
 }
 
-
 async function getSanityData() {
-  const clientData = getPosts("home")
+  const clientData = getPosts('home')
 
- const data = await clientData
-//  console.log(data)
-  return data;
- 
+  const data = await clientData
+  //  console.log(data)
+  return data
 }
