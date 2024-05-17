1d8a5a80b26571b4894a96ea62b94d209367e471
shikhars16 <sabharwal.shikhar@gmail.com>
2024-05-17 14:14:13 -0400

    Fixed contactsection; Made the red rectangles stretch out to the sides of the screen. Had to remove container property and apply padding to fadein property

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -127,7 +127,6 @@ export default async function About() {
         intro="Our team of experienced designers and developers has just one thing on their mind; working on your ideas to draw a smile on the face of your users worldwide. From conducting Brand Sprints to UX Design."
         pages={blogArticles}
       />
-
       <ContactSection contactData={contactData} />
     </>
   )
