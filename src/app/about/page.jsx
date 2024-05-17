343651827ef91138179a656d232c32f140668781
shikhars16 <sabharwal.shikhar@gmail.com>
2024-05-17 13:32:51 -0400

    Removed round edges from red rectangles, changed metadata from studio to rounddigital

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -15,7 +15,7 @@ import { getPosts, urlFor } from '@/utils/sanity'
 
 function Culture({data}) {
   return (
-    <div className="mt-24 rounded-4xl bg-[#e14242] py-24 sm:mt-32 lg:mt-40 lg:py-32">
+    <div className="mt-24 bg-[#e14242] py-24 sm:mt-32 lg:mt-40 lg:py-32">
       <SectionIntro
         eyebrow={data.title}
         title={data.heading}
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -4,7 +4,7 @@ export function Container({ as, className, children }) {
   let Component = as ?? 'div'
 
   return (
-    <Component className={clsx('mx-auto max-w-7xl px-6 lg:px-8', className)}>
+    <Component className={clsx('mx-auto max-w-7xl px-6 lg:px-8 py-4', className)}>
       <div className="mx-auto max-w-2xl lg:max-w-none">{children}</div>
     </Component>
   )
